a5fe7b627e5cbbab7af9717ec5389d20ec80722e
Im-Hul <blashirk@gmail.com>
2018-11-27 23:12:06 +0200

    homework is done

--- source/basic-redux/components/Gallery/index.js
+++ source/basic-redux/components/Gallery/index.js
@@ -1,28 +1,53 @@
 // Core
 import React, { Component } from 'react';
 import { hot } from 'react-hot-loader';
-import photo1 from '../../../theme/assets/photos/1.jpeg';
+import cx from 'classnames';
+
+// Store
+import { store } from '../../init/store';
 
 // Instruments
 import Styles from './styles.m.css';
 
-@hot(module)
+// Actions
+import { showNextPhoto, showPrevPhoto, showSelectedPhoto } from './../../bus/gallery/actions';
+
+// @hot(module)
 export default class Gallery extends Component {
+    _showNextPhoto = () => {
+        store.dispatch(showNextPhoto());
+        this.forceUpdate()
+    }
+    _showPrevPhoto = () => {
+        store.dispatch(showPrevPhoto());
+        this.forceUpdate()
+    }
+    _showSelectedPhoto = (event) => {
+        store.dispatch(showSelectedPhoto(event.target.value));
+        this.forceUpdate()
+    }
     render () {
-        const url = photo1;
+        const { gallery: { photos, selectedPhotoIndex } } = store.getState();
+        // const state = store.getState();
+        const photo = photos.find( ( _, index ) => index === selectedPhotoIndex );
+
+        const buttonActiveClass1 = cx({ [Styles.buttonActive]: selectedPhotoIndex === 0 });
+        const buttonActiveClass2 = cx({ [Styles.buttonActive]: selectedPhotoIndex === 1 });
+        const buttonActiveClass3 = cx({ [Styles.buttonActive]: selectedPhotoIndex === 2 });
+        const buttonActiveClass4 = cx({ [Styles.buttonActive]: selectedPhotoIndex === 3 });
+
+        // console.info("::::state::::", state);
 
         return (
             <section className = { Styles.gallery }>
-                <img src = { url } />
+                <img src = { photo.url } />
                 <div>
-                    <button>←</button>
-                    <button className = { Styles.buttonActive } value = '0'>
-                        1
-                    </button>
-                    <button value = '1'>2</button>
-                    <button value = '2'>3</button>
-                    <button value = '3'>4</button>
-                    <button>→</button>
+                    <button onClick={ this._showPrevPhoto }>←</button>
+                    <button onClick={ this._showSelectedPhoto } className={ buttonActiveClass1 } value = '0'>1</button>
+                    <button onClick={ this._showSelectedPhoto } className={ buttonActiveClass2 } value = '1'>2</button>
+                    <button onClick={ this._showSelectedPhoto } className={ buttonActiveClass3 } value = '2'>3</button>
+                    <button onClick={ this._showSelectedPhoto } className={ buttonActiveClass4 } value = '3'>4</button>
+                    <button onClick={ this._showNextPhoto }>→</button>
                 </div>
             </section>
         );
---
+++ source/basic-redux/init/store.js
@@ -0,0 +1,30 @@
+import { createStore, applyMiddleware, compose } from 'redux';
+import { createLogger } from 'redux-logger';
+
+import { rootReducer } from './rootReducer';
+
+const logger = createLogger({
+    duration: true,
+    collapsed: true,
+    colors: {
+        title: () =>     '#138bfe',
+        prevState: () => '#1c5faf',
+        action: () =>    '#149945',
+        nextState: () => '#a47104',
+        error: () =>     '#ff0005'
+    }
+});
+
+const preloadedState = JSON.parse(localStorage.getItem( 'gallery' ) );
+const devtools = window.__REDUX_DEVTOOLS_EXTENTION_COMPOSE__;
+const composerEnchancer = devtools ? devtools : compose;
+const enhancedStore = composerEnchancer( applyMiddleware( logger ) );
+
+export const store = preloadedState ?
+createStore( rootReducer, preloadedState, enhancedStore ) :
+createStore( rootReducer, enhancedStore );
+
+store.subscribe( () => {
+    const state = store.getState();
+    localStorage.setItem( 'gallery', JSON.stringify( state ) )
+});
