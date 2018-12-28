9010c1de58bd316f8187ca0261b1effabcea1767
Im-Hul <blashirk@gmail.com>
2018-12-28 18:53:52 +0200

    in progress

--- source/bus/auth/__tests__/signup.saga.js
+++ source/bus/auth/__tests__/signup.saga.js
@@ -1,12 +1,25 @@
 // Core
-import { put, apply } from 'redux-saga/effects';
-import { cloneableGenerator } from 'redux-saga/utils';
+import {
+    put,
+    apply
+} from 'redux-saga/effects';
+import {
+    cloneableGenerator
+} from 'redux-saga/utils';
 
 // Instruments
-import { api } from '../../../REST';
-import { authActions } from '../../auth/actions';
-import { uiActions } from '../../ui/actions';
-import { signup } from '../saga/workers';
+import {
+    api
+} from '../../../REST';
+import {
+    authActions
+} from '../../auth/actions';
+import {
+    uiActions
+} from '../../ui/actions';
+import {
+    signup
+} from '../saga/workers';
 
 const signupAction = authActions.signupAsync(__.userProfile);
 
@@ -14,7 +27,25 @@ const saga = cloneableGenerator(signup)(signupAction);
 let clone = null;
 
 describe('signup saga', () => {
-    test('should dispatch "startFetching" action', () => {
-        expect(saga.next().value).toEqual(put(uiActions.startFetching()));
-    })
+    describe('should pass until response received', () => {
+        test('should dispatch "startFetching" action', () => {
+            expect(saga.next().value).toEqual(put(uiActions.startFetching()));
+        });
+
+        test('should call a fetch request', () => {
+            expect(saga.next().value).toEqual(apply(api, api.auth.signup, [__.userProfile]));
+            clone = saga.clone();
+        });
+    });
+
+    describe('should pass until response received', () => {
+        test('should dispatch "startFetching" action', () => {
+            expect(saga.next().value).toEqual(put(uiActions.startFetching()));
+        });
+
+        test('should call a fetch request', () => {
+            expect(saga.next().value).toEqual(apply(api, api.auth.signup, [__.userProfile]));
+            clone = saga.clone();
+        });
+    });
 });
